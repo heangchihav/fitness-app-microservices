f8cc8ca6c65113bac18977b78d1f338b4a2ac18d
Heang Chihav <heangchihav@gmail.com>
2026-03-03 03:32:45 +0700

    updated spring actuator

--- backend/infrastructure/gateway/src/main/java/com/todo/gateway/SecurityConfig.java
+++ backend/infrastructure/gateway/src/main/java/com/todo/gateway/SecurityConfig.java
@@ -20,11 +20,10 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         return http
-            .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchange -> exchange
-//                        .pathMatchers("/actuator/*").permitAll()
-                        .anyExchange().authenticated()
-                )
+                        .pathMatchers("/actuator/**").permitAll()
+                        .anyExchange().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
